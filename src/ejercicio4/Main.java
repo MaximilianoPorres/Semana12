db1c78a8a5d062029873759937aa257c64c72936
MaximilianoPorres <>
2017-12-08 20:40:02 -0300

    Ejercicio 4 terminado

---
+++ src/ejercicio4/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio4;
+
+/**
+ *
+ * @author Maxi
+ */
+public class Main {
+    
+    /*
+        4, Suponiendo que los siguientes ejemplos, son nombres de clases definidas para objetos, 
+        indique cuáles podrían ser sus superclases:
+        a). cuervo                  superclase: Ave
+        b). médico                  superclase: Persona
+        c). camión                  superclase: Vehiculo
+        d). sube_y_baja             superclase: Juego
+        e). jugador_de_futbol       superclase: Persona
+        f). árbol_frutal            superclase: Planta
+        g). luciérnaga              superclase: Insecto
+    */
+    
+}
